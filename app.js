02309e6d6a63645779bd5c51df15a864b4f5e51a
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-06 19:11:49 +0300

    Add layout for fullscreen grid

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 let gridColumns = 16;
 const sqrGridSize = Math.pow(gridColumns, 2)
+
 function generateGrid(gridSize, gridContainer) {
     for(let i = 0; i < gridSize; i++) {
         const div = document.createElement("div");
@@ -8,3 +9,13 @@ function generateGrid(gridSize, gridContainer) {
     }
 }
 generateGrid(sqrGridSize, container);
+
+function setDivSize(gridColumns){
+    const divSize = 99/gridColumns;
+    const divs = document.querySelectorAll("#container div");
+    for (let div of divs) {
+        div.style.width = `${divSize}%`;
+        div.style.height = `${divSize}vh`
+    }
+}
+setDivSize(gridColumns);
