24a3cd37a58b4fe7834c63b9a59ff88f0787a871
Zivanai <zivanaiarnolddoma@gmail.com>
2022-07-07 16:50:47 +0300

    Add resize grid to add or reduce the number of boxes
    Since the grid regeration is taxing the generate grid function was also
    modified so that it can calculate and set the box size for the grid as
    it is being generated

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const container = document.querySelector("#container");
-let gridColumns = 16;
-const sqrGridSize = Math.pow(gridColumns, 2)
-generateGrid(sqrGridSize, container); //create div before the beginning of dom manipultions
+let gridSize = {"rows": 16, "columns": 16};
+generateGrid(gridSize, container); //create div before the beginning of dom manipultions
 const divs = document.querySelectorAll("#container div");
 const color = "#000000"
 
-function generateGrid(gridSize, gridContainer) {
-    for(let i = 0; i < gridSize; i++) {
+function generateGrid(size, container) {
+    const area = size.rows * size.columns;
+
+    //calculate element size
+    const divWidth = 100/size.columns;
+    const divHeight = 100/size.rows;
+
+    for(let i = 0; i < area; i++) {
         const div = document.createElement("div");
-        gridContainer.appendChild(div);
+
+        //set element size
+        div.style.width = `${divWidth}%`;
+        div.style.height = `${divHeight}vh`;
+
+        //add paint effect on over
+        div.addEventListener("mouseover", ()=> paintGrid(div, color));
+        container.appendChild(div);
     }
 }
 
-function setElementSize(gridColumns, elements = divs){
-    const divSize = 99/gridColumns;
-    for (let element of elements) {
-        element.style.width = `${divSize}%`;
-        element.style.height = `${divSize}vh`
-    }
-}
-setElementSize(gridColumns);
-
-divs.forEach((div) => div.addEventListener("mouseover", ()=> paintGrid(div, color)))
 function paintGrid(div, color){
     div.style.backgroundColor = color;
+}
+
+const promptBtn = document.querySelector("#gridSizePrompt");
+promptBtn.addEventListener("click", generateNewGrid);
+function generateNewGrid(){
+    getGridSize();
+    container.innerHTML = "";
+    generateGrid(gridSize, container);
+}
+function getGridSize(){
+    let cols = parseInt(prompt("Enter number of columns:"));
+    columns = (cols > 100)? 100: cols;
+    gridSize.columns = columns;
+    gridSize.rows = columns
 }
